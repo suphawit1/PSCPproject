55ab9ddd8bfdabb95fb35e8a575fe2778b5c9986
Markkerg <118672468+Markkerg@users.noreply.github.com>
2022-11-21 18:33:14 +0700

    Update Fighter.py

--- Fighter.py
+++ Fighter.py
@@ -1,9 +1,9 @@
 import pygame
 
 class Fighter():
-    def _init_(self, x, y):
+    def __init__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
 
 
-def draw(self, surface):
-    pygame.draw.rect(surface, (255, 0, 0), self.rect)
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)
