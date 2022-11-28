b04ed4d55bc6af7d83f0010ed93a1bc48787b372
Guntbuster06 <109967528+Guntbuster06@users.noreply.github.com>
2022-11-28 15:45:31 +0700

    Attacking
    สมิทธิพงศ์ จูปรางค์ 65070230

--- Fighter.py
+++ Fighter.py
@@ -1,13 +1,15 @@
 import pygame
 
 class Fighter():
-    def __init__(self, x, y):
+    def __initpp__(self, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attacking = False
+        self.attack_type = 0
     
 
-    def move(self, screen_width, screen_height):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -16,17 +18,26 @@ class Fighter():
         #keypress
 
         key = pygame.key.get_pressed()
-
-        #movement
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
-        #jump
-        if key[pygame.K_w] and self.jump == False:
-            self.vel_y = -30
-            self.jump = True
-            
+        #can only perfrom other action if not currently attacking
+        if self.attacking == False:
+            #movement
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+            #jump
+            if key[pygame.K_w] and self.jump == False:
+                self.vel_y = -30
+                self.jump = True
+            #attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                #which attack type were used
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
+        #apply gravity    
         self.vel_y += GRAVITY
         dy += self.vel_y
         
@@ -42,6 +53,14 @@ class Fighter():
         
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            print("HIT")
+        
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
 
     def draw(self, surface):
---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+from Fighter import Fighter
+
+pygame.init()
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Battle of Stickman")
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+bg_image = pygame.image.load("assets/bg2.png").convert_alpha()
+
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
+run = True
+while run:
+
+    clock.tick(FPS)
+
+    draw_bg()
+
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+    #fighter_2.move()
+    
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    pygame.display.update()
+pygame.quit()
