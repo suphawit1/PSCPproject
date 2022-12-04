f54970450dd342336b7fc2868fc593859a64641f
Suphawit Pattamapassakun <112681287+suphawit1@users.noreply.github.com>
2022-12-04 14:22:07 +0700

    Add demo Sprite sheet

--- Fighter.py
+++ Fighter.py
@@ -1,22 +1,46 @@
 import pygame
 
 class Fighter():
-    def __init__(self, x, y):
-        self.flip = False
+    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
+        self.running = False
         self.jump = False
         self.attacking = False
         self.attack_type = 0
-        self.health = 100
+        self.attack_cooldown = 0
+        self.hit = False
+        self.health = 10
+        self.alive = True
     
+
+    def load_images(self, sprite_sheet, animation_steps):
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
+
 
     def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
         dy = 0
-
+        self.running = False
+        self.attack_type = 0
         #keypress
 
         key = pygame.key.get_pressed()
@@ -25,8 +49,10 @@ class Fighter():
             #movement
             if key[pygame.K_a]:
                 dx = -SPEED
+                self.running = True
             if key[pygame.K_d]:
                 dx = SPEED
+                self.running = True
             #jump
             if key[pygame.K_w] and self.jump == False:
                 self.vel_y = -30
@@ -58,18 +84,74 @@ class Fighter():
             self.flip = False
         else:
             self.flip = True
+        
+        #attack cooldown
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+
         #update player position
         self.rect.x += dx
         self.rect.y += dy
 
+    #animation update
+    def update(self):
+        #check action
+        if self.health <= 0:
+            self.health = 0
+            self.alive = False
+            self.update_action(6)
+        elif self.hit == True:
+            self.update_action(5)
+        elif self.attacking == True:
+            if self.attack_type == 1:
+                self.update_action(3)
+            elif self.attack_type == 2:
+                self.update_action(4)
+        elif self.jump == True:
+            self.update_action(2)
+        elif self.running == True:
+            self.update_action(1)
+        else:
+            self.update_action(0)
+        animation_cooldow = 70
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldow:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animation_list[self.action]):
+            #if player dead
+            if self.alive == False:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
+                #if attack was executed
+                if self.action == 3 or self.action == 4:
+                    self.attacking = False
+                    self.attack_cooldown = 50
+                #check if damage was taken
+                if self.action == 5:
+                    self.hit = False
+                    #if was player in middle of attck
+                    self.attacking = False
+                    self.attack_cooldown = 50
+
     def attack(self, surface, target):
-        self.attacking = True
-        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
-        if attacking_rect.colliderect(target.rect):
-            target.health -= 10
-        
-        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+        if self.attack_cooldown == 0:
+            self.attacking = True
+            attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+            if attacking_rect.colliderect(target.rect):
+                target.health -= 10
+                target.hit = True
+            #pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        img = pygame.transform.flip(self.image, self.flip, False)
+        #pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
--- main.py
+++ main.py
@@ -16,8 +16,18 @@ Red = (255, 0, 0)
 Yellow = (255, 255, 0)
 White = (255, 255, 255)
 
+#define fighter cariable
+STICKMAN_SIZE = 348
+STICKMAN_SCALE = 0.7
+STICKMAN_OFFSET = [120, 80]
+STICKMAN_DATA = [STICKMAN_SIZE, STICKMAN_SCALE, STICKMAN_OFFSET]
 bg_image = pygame.image.load("assets/bg2.png").convert_alpha()
 
+#sheet load
+test_sheet = pygame.image.load("assets/demostick/sheetdemo.png").convert_alpha()
+
+#set step animation
+TEST_ANIMATION_STEP = [7, 5, 1, 4, 4, 3, 6]
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
@@ -29,8 +39,8 @@ def draw_health_bar(hrealth, x, y):
     pygame.draw.rect(screen, Red, (x, y, 400, 30))
     pygame.draw.rect(screen, Yellow, (x, y, 400 * ratio, 30))
 
-fighter_1 = Fighter(200, 310)
-fighter_2 = Fighter(700, 310)
+fighter_1 = Fighter(200, 310, False, STICKMAN_DATA, test_sheet, TEST_ANIMATION_STEP)
+fighter_2 = Fighter(700, 310, True, STICKMAN_DATA, test_sheet, TEST_ANIMATION_STEP)
 run = True
 while run:
 
@@ -45,6 +55,10 @@ while run:
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
     #fighter_2.move()
     
+    #update fighter
+    fighter_1.update()
+    fighter_2.update()
+
     fighter_1.draw(screen)
     fighter_2.draw(screen)
 
