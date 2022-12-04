f44fc679dfdf5600a69404257cb0d1ccb538b088
Suphawit Pattamapassakun <112681287+suphawit1@users.noreply.github.com>
2022-12-04 14:24:22 +0700

    Update Animation

--- Fighter.py
+++ Fighter.py
@@ -19,7 +19,7 @@ class Fighter():
         self.attack_type = 0
         self.attack_cooldown = 0
         self.hit = False
-        self.health = 10
+        self.health = 100
         self.alive = True
     
 
